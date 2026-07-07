1cd894609770b4d46ec4a37e14b6399bd9b5280b
kodiitulip <kodii.tulip@proton.me>
2026-07-07 14:01:12 -0300

    kdeconnect

--- nixos/features/desktop/kde.nix
+++ nixos/features/desktop/kde.nix
@@ -1,11 +1,15 @@
 { self, ... }:
 {
-  flake.nixosModules.kde = {
-    imports = [ self.nixosModules.desktop ];
-    services = {
-      displayManager.sddm.enable = true;
-      desktopManager.plasma6.enable = true;
+  flake.nixosModules.kde =
+    { pkgs, ... }:
+    {
+      imports = [ self.nixosModules.desktop ];
+      services = {
+        displayManager.sddm.enable = true;
+        desktopManager.plasma6.enable = true;
+      };
+      security.rtkit.enable = true;
+      environment.systemPackages = [ pkgs.kdePackages.kdeconnect-kde ];
+      programs.kdeconnect.enable = true;
     };
-    security.rtkit.enable = true;
-  };
 }
