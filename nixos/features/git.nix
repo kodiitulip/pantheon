3241d217e85a5fce98a9e527f5a04f8850a127ff
kodiitulip <kodii.tulip@proton.me>
2026-07-08 00:38:16 -0300

    downgrading gh

--- nixos/features/git.nix
+++ nixos/features/git.nix
@@ -2,7 +2,21 @@
   flake.nixosModules.git =
     { pkgs, ... }:
     {
-      environment.systemPackages = with pkgs; [ gh ];
+      nixpkgs.overlays = [
+        (self: super: {
+          gh = super.gh.overrideAttrs rec {
+            version = "2.95.0";
+            src = super.fetchFromGitHub {
+              owner = "cli";
+              repo = "cli";
+              tag = "v${version}";
+              hash = "sha256-Hzdod8dJuwFv3mNa4Nflqf8uy45RpoeO93sFSMq3D5E=";
+            };
+            vendorHash = "sha256-tqbo791t7phe6ip5UzBiLer0rGcKqpKGF0bqwxr3j78=";
+          };
+        })
+      ];
+      environment.systemPackages = with pkgs; [ (gh) ];
       programs = {
         lazygit = {
           enable = true;
@@ -13,7 +27,12 @@
         };
         git = {
           enable = true;
+          package = pkgs.git.override { withLibsecret = true; };
           config = {
+            credential.helper = "libsecret";
+            push = {
+              autoSetupRemote = true;
+            };
             init = {
               defaultBranch = "main";
             };
