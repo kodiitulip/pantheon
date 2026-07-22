9414391ee5ebce99ed17de63f946f47e25935e94
kodiitulip <kodii.tulip@proton.me>
2026-07-21 23:26:12 -0300

    enable bluetooth

--- nixos/hosts/persephone/configuration.nix
+++ nixos/hosts/persephone/configuration.nix
@@ -4,59 +4,51 @@
     modules = [ self.nixosModules.persephone ];
   };
 
-  flake.nixosModules.persephone =
-    # { pkgs, ... }:
-    {
-      imports = with self.nixosModules; [
-        base
-        console
+  flake.nixosModules.persephone = {
+    imports = with self.nixosModules; [
+      base
+      console
 
-        nix
-        git
+      nix
+      git
 
-        kde
+      kde
 
-        gaming
+      gaming
 
-        obs-studio
-        music
-        art
-      ];
+      obs-studio
+      music
+      art
+    ];
 
-      networking.hostName = "persephone";
-      services = {
-        openssh.enable = true;
-        flatpak.enable = true;
-      };
-
-      # users.users.kodie = {
-      #   isNormalUser = true;
-      #   description = "kodie";
-      #   extraGroups = [
-      #     "networkmanager"
-      #     "wheel"
-      #   ];
-      #   initialPassword = "kodie";
-      #   shell = pkgs.nushell;
-      # };
-
-      preferences.user = {
-        enable = true;
-        enableHjemUser = true;
-        name = "kodie";
-        face = ./avatar.png;
-      };
-
-      programs = {
-        firefox.enable = true;
-      };
-      hardware.uinput.enable = true;
-      boot.kernelModules = [ "uinput" ];
-
-      nixpkgs.config.allowUnfree = true;
-
-      environment.systemPackages = [ ];
-
-      system.stateVersion = "26.05"; # WARN: No changing wili nilli
+    networking.hostName = "persephone";
+    services = {
+      openssh.enable = true;
+      flatpak.enable = true;
     };
+
+    preferences.user = {
+      enable = true;
+      enableHjemUser = true;
+      name = "kodie";
+      face = ./avatar.png;
+    };
+
+    programs = {
+      firefox.enable = true;
+    };
+    hardware.uinput.enable = true;
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = false;
+    };
+
+    boot.kernelModules = [ "uinput" ];
+
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = [ ];
+
+    system.stateVersion = "26.05"; # WARN: No changing wili nilly
+  };
 }
