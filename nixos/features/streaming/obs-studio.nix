ce7cb63190e03c6c8d9f51657be8d336ad0626d0
kodiitulip <kodii.tulip@proton.me>
2026-08-06 13:51:20 -0300

    grab obs-tuna from a hash with mpris fix

--- nixos/features/streaming/obs-studio.nix
+++ nixos/features/streaming/obs-studio.nix
@@ -1,6 +1,10 @@
+{ self, ... }:
 {
   flake.nixosModules.obs-studio =
     { pkgs, ... }:
+    let
+      system = pkgs.stdenv.hostPlatform.system;
+    in
     {
       programs.obs-studio = {
         enable = true;
@@ -12,9 +16,58 @@
           advanced-scene-switcher
           obs-scene-as-transition
           obs-vkcapture
-          obs-tuna
+          # obs-tuna
+          self.packages.${system}.obs-tuna
         ];
         enableVirtualCamera = true;
       };
+    };
+
+  perSystem =
+    { pkgs, lib, ... }:
+    {
+      packages.obs-tuna = pkgs.stdenv.mkDerivation (finalAttrs: {
+        pname = "obs-tuna";
+        version = "1.9.11";
+
+        nativeBuildInputs = with pkgs; [
+          cmake
+          pkg-config
+          qt6Packages.wrapQtAppsHook
+          git
+        ];
+        buildInputs = with pkgs; [
+          obs-studio
+          qt6Packages.qtbase
+          zlib
+          curl
+          dbus
+          taglib
+          libmpdclient
+          utf8cpp
+        ];
+
+        cmakeFlags = [
+          "-DFETCHCONTENT_SOURCE_DIR_TAGLIB=${pkgs.taglib.src}"
+        ];
+
+        src = pkgs.fetchFromGitHub {
+          owner = "univrsal";
+          repo = "tuna";
+          rev = "1a13ce5db9e24e7bddfdd5be711505ee3363f003";
+          hash = "sha256-gzwqPEwqM7YPArU5n6P7fWbKlEwfYXIxDhRgMyTaodg=";
+          fetchSubmodules = true;
+        };
+
+        dontWrapQtApps = true;
+
+        meta = {
+          description = "Song information plugin for obs-studio";
+          homepage = "https://github.com/univrsal/tuna";
+          license = lib.licenses.gpl2Only;
+          maintainers = with lib.maintainers; [ shortcord ];
+          platforms = lib.platforms.linux;
+        };
+      });
     };
 }
