546e696d5f5ada15b692959f0fa52f4d91dd7d7b
kodiitulip <kodii.tulip@proton.me>
2026-09-17 15:52:05 -0300

    sla

--- nixos/features/streaming/music.nix
+++ nixos/features/streaming/music.nix
@@ -4,28 +4,35 @@
     { pkgs, config, ... }:
     let
       spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+      username = config.preferences.user.name;
     in
     {
-      imports = [ inputs.spicetify-nix.nixosModules.spicetify ];
-
-      hjem.users.${config.preferences.user.name}.packages = with pkgs; [
-        pear-desktop
-        config.programs.spicetify.spicedSpotify
-      ];
-
-      programs.spicetify = {
-        enable = false;
-        enabledExtensions = with spicePkgs.extensions; [
-          adblockify
-          shuffle
-          history
-          {
-            src = ./tuna-spicetify;
-            name = "tunaSpicetify.js";
-          }
-        ];
-        theme = spicePkgs.themes.text;
-        colorScheme = "RosePineMoon";
+      hjem = {
+        extraModules = [ inputs.spicetify-nix.hjemModules.default ];
+        users.${username} = {
+          programs.spicetify = {
+            enable = true;
+            enabledExtensions = with spicePkgs.extensions; [
+              adblockify
+              shuffle
+              history
+              {
+                src = ./tuna-spicetify;
+                name = "tunaSpicetify.js";
+              }
+            ];
+            enabledCustomApps = with spicePkgs.apps; [
+              marketplace
+              lyricsPlus
+            ];
+            enabledSnippets = [ ];
+            theme = spicePkgs.themes.text;
+            colorScheme = "RosePineMoon";
+          };
+          packages = with pkgs; [
+            pear-desktop
+          ];
+        };
       };
     };
 }
