c55d72c35efb99a2c051f0d76802205987e272da
kodiitulip <kodii.tulip@proton.me>
2026-09-22 15:25:03 -0300

    the hell

--- nixos/features/base/locale.nix
+++ nixos/features/base/locale.nix
@@ -1,37 +1,34 @@
 {
-  flake.nixosModules.base =
-    { pkgs, ... }:
-    {
-      boot = {
-        loader = {
-          systemd-boot.enable = true;
-          efi.canTouchEfiVariables = true;
-        };
-        kernelPackages = pkgs.linuxPackages_latest;
+  flake.nixosModules.base = {
+    boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
       };
-
-      networking.networkmanager.enable = true;
-
-      services.xserver.xkb = {
-        layout = "br";
-        variant = "nodeadkeys";
-        options = "compose:rctrl";
-      };
-
-      time.timeZone = "America/Fortaleza";
-      i18n.defaultLocale = "en_US.UTF-8";
-      i18n.extraLocaleSettings = {
-        LC_ADDRESS = "pt_BR.UTF-8";
-        LC_IDENTIFICATION = "pt_BR.UTF-8";
-        LC_MEASUREMENT = "pt_BR.UTF-8";
-        LC_MONETARY = "pt_BR.UTF-8";
-        LC_NAME = "pt_BR.UTF-8";
-        LC_NUMERIC = "pt_BR.UTF-8";
-        LC_PAPER = "pt_BR.UTF-8";
-        LC_TELEPHONE = "pt_BR.UTF-8";
-        LC_TIME = "pt_BR.UTF-8";
-      };
-
-      console.keyMap = "br-abnt2";
     };
+
+    networking.networkmanager.enable = true;
+
+    services.xserver.xkb = {
+      layout = "br";
+      variant = "nodeadkeys";
+      options = "compose:rctrl";
+    };
+
+    time.timeZone = "America/Fortaleza";
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+
+    console.keyMap = "br-abnt2";
+  };
 }
